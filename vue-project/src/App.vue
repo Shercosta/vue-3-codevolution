2b1e75bb741d44a50ad1e9bff2e4d8c9be5c77b9
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-01-30 11:25:30 +0700

    tutorial 8

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,11 +4,25 @@
 </script>
 
 <template>
-  <div>{{ greet }} {{ name }}</div>
-  <div v-html="channel"></div>
-  <div v-html="hack"></div>
   <h2 v-bind:id="headingId">Heading</h2>
   <button v-bind:disabled="isDisabled">Bind</button>
+  <h2 class="underline">Underlined Text</h2>
+  <h2 v-bind:class="status" class="underline">Status</h2>
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
+  <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">
+    array conditional Movie
+  </h2>
+  <h2
+    v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldout,
+      'sold-out': isSoldout,
+    }"
+  >
+    Object Conditioal Movie
+  </h2>
 </template>
 
 <script>
@@ -16,12 +30,11 @@ export default {
   name: "App",
   data() {
     return {
-      greet: "Hello",
-      name: "Batman",
-      channel: "<b>Codevolution</b>",
-      hack: `<a href="#" onclick="alert('You have been hacked')"> win a prize! </a>`,
       headingId: "heading",
       isDisabled: false,
+      status: "danger",
+      isPromoted: true,
+      isSoldout: true,
     };
   },
 };
@@ -35,5 +48,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: green;
+}
+
+.sold-out {
+  color: red;
 }
 </style>
