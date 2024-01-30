b6b57180af912f397a09856572489b9458818603
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-01-30 11:39:49 +0700

    tutorial 9

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,25 +4,44 @@
 </script>
 
 <template>
-  <h2 v-bind:id="headingId">Heading</h2>
-  <button v-bind:disabled="isDisabled">Bind</button>
-  <h2 class="underline">Underlined Text</h2>
-  <h2 v-bind:class="status" class="underline">Status</h2>
-  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
-  <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
-  <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
-  <h2 v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">
-    array conditional Movie
-  </h2>
-  <h2
-    v-bind:class="{
-      promoted: isPromoted,
-      new: !isSoldout,
-      'sold-out': isSoldout,
-    }"
-  >
-    Object Conditioal Movie
-  </h2>
+  <div>
+    <h2 v-bind:id="headingId">Heading</h2>
+    <button v-bind:disabled="isDisabled">Bind</button>
+    <h2 class="underline">Underlined Text</h2>
+    <h2 v-bind:class="status" class="underline">Status</h2>
+    <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+    <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
+    <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
+    <h2
+      v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']"
+    >
+      array conditional Movie
+    </h2>
+    <h2
+      v-bind:class="{
+        promoted: isPromoted,
+        new: !isSoldout,
+        'sold-out': isSoldout,
+      }"
+    >
+      Object Conditioal Movie
+    </h2>
+    <h2
+      v-bind:style="{
+        color: highlightColor,
+        fontSize: headerSize + 'px',
+        padding: '20px',
+      }"
+    >
+      Inline Style
+    </h2>
+    <h2 v-bind:style="headerStyleObject">Style Object</h2>
+
+    <div v-bind:style="[baseStyleObject, successStyleObject]">
+      Success Style
+    </div>
+    <div v-bind:style="[baseStyleObject, dangerStyleObject]">Danger Style</div>
+  </div>
 </template>
 
 <script>
@@ -35,6 +54,28 @@ export default {
       status: "danger",
       isPromoted: true,
       isSoldout: true,
+      highlightColor: "orange",
+      headerSize: 50,
+      headerStyleObject: {
+        color: "orange",
+        fontSize: "50px",
+        padding: "20px",
+      },
+      baseStyleObject: {
+        fontSize: "50px",
+        padding: "10px",
+      },
+      successStyleObject: {
+        color: "green",
+        backgroundColor: "lightgreen",
+        border: "1px solid green",
+        padding: "20px",
+      },
+      dangerStyleObject: {
+        color: "darkred",
+        backgroundColor: "red",
+        border: "1px solid darkred",
+      },
     };
   },
 };
