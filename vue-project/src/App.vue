802c2679e05b4f66e441872cc6f5fb951bbe98f4
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-02-02 15:13:53 +0700

    tutorial 14

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,66 +1,26 @@
-<script setup>
-// import HelloWorld from "./components/HelloWorld.vue";
-// import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
-  <div>
-    <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
-    <h2 v-for="name in fullNames" :key="name.first">
-      {{ name.first }} {{ name.last }}
-    </h2>
-    <div v-for="actor in actors" :key="actor.name">
-      <h2>{{ actor.name }}</h2>
-      <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
-    </div>
-
-    <h2 v-for="(value, key, index) in myInfo" :key="index">
-      {{ index }} {{ key }} {{ value }}
-    </h2>
-
-    <template v-for="name in names" :key="name">
-      <h2>{{ name }}</h2>
-      <hr />
-    </template>
-  </div>
+  <template v-for="name in names" :key="name">
+    <h2>{{ name }}</h2>
+    <input placeholder="Last name" />
+    <hr />
+  </template>
+  <button @click="shuffle">Shuffle!</button>
 </template>
 
 <script>
+import _ from "lodash";
 export default {
   name: "App",
   data() {
     return {
-      names: ["Bruce", "Clark", "Diana"],
-      fullNames: [
-        {
-          first: "Bruce",
-          last: "Wayne",
-        },
-        {
-          first: "Clark",
-          last: "Kent",
-        },
-        {
-          first: "Princess",
-          last: "Diana",
-        },
-      ],
-      actors: [
-        {
-          name: "Christian Bale",
-          movies: ["Batman", "The Prestige"],
-        },
-        {
-          name: "Di Caprio",
-          movies: ["Titanic", "Inception"],
-        },
-      ],
-      myInfo: {
-        name: "Vishwas",
-        channel: "Codevolution",
-        course: "Vue 3",
-      },
+      names: ["Bruce", "Clark", "Diana", "Barry"],
     };
+  },
+  methods: {
+    shuffle() {
+      console.log(this.names);
+      this.names = _.shuffle(this.names);
+    },
   },
 };
 </script>
@@ -73,21 +33,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.underline {
-  text-decoration: underline;
-}
-
-.promoted {
-  font-style: italic;
-}
-
-.new {
-  color: green;
-}
-
-.sold-out {
-  color: red;
 }
 </style>
