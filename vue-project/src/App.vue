8d31c480a88da1ec4ab4e8d5abfb808a79922fbc
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-02-02 15:05:31 +0700

    tutorial 13

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,19 +5,23 @@
 
 <template>
   <div>
-    <h2 v-if="num === 0">The number is zero</h2>
-    <h2 v-else-if="num < 0">The number is negative</h2>
-    <h2 v-else-if="num > 0">The number is positive</h2>
-    <h2 v-else>Not a number</h2>
+    <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
+    <h2 v-for="name in fullNames" :key="name.first">
+      {{ name.first }} {{ name.last }}
+    </h2>
+    <div v-for="actor in actors" :key="actor.name">
+      <h2>{{ actor.name }}</h2>
+      <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
+    </div>
 
-    <template v-if="display">
-      <h2>Vishwas</h2>
-      <h2>Codwevolution</h2>
-      <h2>Vue</h2>
+    <h2 v-for="(value, key, index) in myInfo" :key="index">
+      {{ index }} {{ key }} {{ value }}
+    </h2>
+
+    <template v-for="name in names" :key="name">
+      <h2>{{ name }}</h2>
+      <hr />
     </template>
-
-    <h2 v-show="showElement">Using v-show</h2>
-    <h2 v-if="showElement">Using v-if</h2>
   </div>
 </template>
 
@@ -26,9 +30,36 @@ export default {
   name: "App",
   data() {
     return {
-      num: "4",
-      display: true,
-      showElement: false,
+      names: ["Bruce", "Clark", "Diana"],
+      fullNames: [
+        {
+          first: "Bruce",
+          last: "Wayne",
+        },
+        {
+          first: "Clark",
+          last: "Kent",
+        },
+        {
+          first: "Princess",
+          last: "Diana",
+        },
+      ],
+      actors: [
+        {
+          name: "Christian Bale",
+          movies: ["Batman", "The Prestige"],
+        },
+        {
+          name: "Di Caprio",
+          movies: ["Titanic", "Inception"],
+        },
+      ],
+      myInfo: {
+        name: "Vishwas",
+        channel: "Codevolution",
+        course: "Vue 3",
+      },
     };
   },
 };
