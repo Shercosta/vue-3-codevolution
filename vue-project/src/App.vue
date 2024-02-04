a3ab9caa0ab51d6c6507c0a501a375c18f98c634
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-02-04 16:47:51 +0700

    tutorial 16

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div>
-    <template v-for="name in names" :key="name">
-      <h2 v-if="name === 'Bruce'">{{ name }}</h2>
-    </template>
+    <h2>{{ 2 + 3 + 5 }}</h2>
+    <h2>{{ 5 + 10 + 15 }}</h2>
+    <h2>Add method - {{ add(10, 15, 20) }}</h2>
+    <h2>Multiply method - {{ multiply(baseValue) }}</h2>
   </div>
 </template>
 
 <script>
-import _ from "lodash";
 export default {
   name: "App",
   data() {
     return {
-      names: ["Bruce", "Clark", "Diana", "Barry"],
+      baseMultiplier: 5,
+      baseValue: 2,
     };
+  },
+  methods: {
+    add(a, b, c) {
+      return a + b + c;
+    },
+    multiply(num) {
+      return num * this.baseMultiplier;
+    },
   },
 };
 </script>
