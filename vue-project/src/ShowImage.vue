0219f337c1eaf50b5c6ba0a31e5b29f34b9824a6
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-02-15 19:13:52 +0700

    side project: showtext/image

---
+++ vue-project/src/ShowImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="toggleShow()">Click me!</button>
+    <div v-show="showText">
+      <h2>This Text</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShowImage",
+  data() {
+    return {
+      showText: true,
+    };
+  },
+  methods: {
+    toggleShow() {
+      this.showText = !this.showText;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,5 +4,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import BonusDirectives from "./BonusDirectives.vue";
 import ComputedProperties from "./ComputedProperties.vue";
+import showImage from "./ShowImage.vue";
 
-createApp(ComputedProperties).mount("#app");
+createApp(showImage).mount("#app");
