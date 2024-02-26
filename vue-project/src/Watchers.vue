797f071a5d0e830085aee2734b3e7b066437378c
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-02-26 10:39:52 +0700

    tutorial 28 immediate & deep watcherz

--- vue-project/src/Watchers.vue
+++ vue-project/src/Watchers.vue
@@ -6,6 +6,16 @@
       <button @click="volume += 2">Increase</button>
       <button @click="volume -= 2">Decrease</button>
     </div>
+
+    <input type="text" name="" v-model="movie" id="" />
+    <input type="text" name="" v-model="movieInfo.title" id="" />
+    <input type="text" name="" v-model="movieInfo.actor" id="" />
+
+    <div>
+      <button @click="movieList = movieList.concat(['Wonder Woman'])">
+        Add Movie
+      </button>
+    </div>
   </div>
 </template>
 
@@ -15,19 +25,42 @@ export default {
   data() {
     return {
       volume: 0,
+      movie: "Batman",
+      movieInfo: {
+        title: "",
+        actor: "",
+      },
+      movieList: ["Batman", "Sumerpan"],
     };
   },
   methods: {},
   computed: {},
   watch: {
     volume(newValue, oldValue) {
-      if(newValue > oldValue && newValue === 16) {
-        alert("Warning more than 16")
+      if (newValue > oldValue && newValue === 16) {
+        alert("Warning more than 16");
       }
-    }
+    },
+    movie: {
+      handler(newValue) {
+        console.log(`Calling API with movie name = ${newValue}`);
+      },
+      immediate: true,
+    },
+    movieInfo: {
+      handler(newValue) {
+        console.log(`TItle = ${newValue.title} actor = ${newValue.actor}`);
+      },
+      deep: true,
+    },
+    movieList: {
+      handler(newValue) {
+        console.log(newValue);
+      },
+      // deep: true,
+    },
   },
 };
 </script>
 
-<style>
-</style>
+<style></style>
